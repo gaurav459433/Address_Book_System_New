c184e4a0e1f3e474c9d1860395dc8a0b7d8a4935
gaurav459433 <gaurav459433@gmail.com>
2023-03-22 01:22:34 +0530

    Added: Code For Add Multiple Person Contact To Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,72 +1,81 @@
 package addressbook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
     Scanner sc = new Scanner(System.in);
-    ContactPerson cp = new ContactPerson();
+    ArrayList<ContactPerson> contactPersonArrayList = new ArrayList<ContactPerson>();
 
     public void addContact() {
+        ContactPerson contactPerson = new ContactPerson();
         System.out.println("Enter First Name: ");
-        cp.setFirstName(sc.next());
+        contactPerson.setFirstName(sc.next());
         System.out.println("Enter Last Name: ");
-        cp.setLastName(sc.next());
+        contactPerson.setLastName(sc.next());
         System.out.println("Enter Address Name: ");
-        cp.setAddress(sc.next());
+        contactPerson.setAddress(sc.next());
         System.out.println("Enter City Name: ");
-        cp.setCity(sc.next());
+        contactPerson.setCity(sc.next());
         System.out.println("Enter State Name: ");
-        cp.setState(sc.next());
+        contactPerson.setState(sc.next());
         System.out.println("Enter Email: ");
-        cp.setEmail(sc.next());
+        contactPerson.setEmail(sc.next());
         System.out.println("Enter Zip Code: ");
-        cp.setZip(sc.nextInt());
+        contactPerson.setZip(sc.nextInt());
         System.out.println("Enter Mobile Number: ");
-        cp.setPhoneNumber(sc.nextLong());
+        contactPerson.setPhoneNumber(sc.nextLong());
 
+        contactPersonArrayList.add(contactPerson);
     }
 
     public void showContact() {
-        System.out.println(cp.toString());
+        for (int i = 0; i < contactPersonArrayList.size(); i++) {
+            ContactPerson contacts = contactPersonArrayList.get(i);
+            System.out.println(contacts.toString());
+
+        }
     }
 
     public void editContact() {
         System.out.println("Enter First Name To Edit:");
         String name = sc.next();
-        if (cp.getFirstName().equals(name)) {
-            System.out.println("Enter First Name: ");
-            cp.setFirstName(sc.next());
-            System.out.println("Enter Last Name: ");
-            cp.setLastName(sc.next());
-            System.out.println("Enter Address Name: ");
-            cp.setAddress(sc.next());
-            System.out.println("Enter City Name: ");
-            cp.setCity(sc.next());
-            System.out.println("Enter State Name: ");
-            cp.setState(sc.next());
-            System.out.println("Enter Email: ");
-            cp.setEmail(sc.next());
-            System.out.println("Enter Zip Code: ");
-            cp.setZip(sc.nextInt());
-            System.out.println("Enter Mobile Number: ");
-            cp.setPhoneNumber(sc.nextLong());
-        } else {
-            System.out.println("Contact Doesn't Exist:");
+        for (ContactPerson cp : contactPersonArrayList) {
+            if (cp.getFirstName().equalsIgnoreCase(name)) {
+                System.out.println("Enter First Name: ");
+                cp.setFirstName(sc.next());
+                System.out.println("Enter Last Name: ");
+                cp.setLastName(sc.next());
+                System.out.println("Enter Address Name: ");
+                cp.setAddress(sc.next());
+                System.out.println("Enter City Name: ");
+                cp.setCity(sc.next());
+                System.out.println("Enter State Name: ");
+                cp.setState(sc.next());
+                System.out.println("Enter Email: ");
+                cp.setEmail(sc.next());
+                System.out.println("Enter Zip Code: ");
+                cp.setZip(sc.nextInt());
+                System.out.println("Enter Mobile Number: ");
+                cp.setPhoneNumber(sc.nextLong());
+            } else {
+                System.out.println("Contact Doesn't Exist:");
+            }
         }
     }
 
     public void deleteContact() {
         System.out.println("Enter First Name To Delete:");
         String name = sc.next();
-        if (cp.getFirstName().equals(name)) {
-            cp.setFirstName(null);
-            cp.setLastName(null);
-            cp.setAddress(null);
-            cp.setCity(null);
-            cp.setState(null);
-            cp.setEmail(null);
-            cp.setZip(0);
-            cp.setPhoneNumber(0);
+        for (ContactPerson cp : contactPersonArrayList) {
+            if (cp.getFirstName().equalsIgnoreCase(name)) {
+                contactPersonArrayList.remove(cp);
+                System.out.println("!!Deleted!!");
+                break;
+            } else {
+                System.out.println("Doesn't exist.");
+            }
+
         }
     }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -7,30 +7,43 @@ public class AddressBookMain {
         System.out.println("Welcome To Address Book Program: ");
         Scanner scan = new Scanner(System.in);
         AddressBook book = new AddressBook();
-        int option;
+        int flag = 1;
 
-        do {
-            System.out.println("1. ADD CONTACT \n2. EDIT CONTACT \n3. DISPLAY CONTACT \n4. DELETE CONTACT ");
-            System.out.println("Enter the Operation Number");
-            option = scan.nextInt();
-            switch (option) {
+        while (flag == 1) {
+            System.out.println("Select a choice : \n1. ADD CONTACT \n2. EDIT CONTACT \n3. DISPLAY CONTACT \n4. DELETE CONTACT \n5. EXIT ");
+            System.out.println("Enter the Operation Number: ");
+            int choice = scan.nextInt();
+
+            switch (choice) {
                 case 1:
                     book.addContact();
                     break;
                 case 2:
+                    if (book.contactPersonArrayList.isEmpty()) {
+                        System.out.println(" Address book is empty ");
+                        break;
+                    }
                     book.editContact();
                     break;
                 case 3:
                     book.showContact();
                     break;
                 case 4:
+                    if (book.contactPersonArrayList.isEmpty()) {
+                        System.out.println(" Address book is empty ");
+                        break;
+                    }
                     book.deleteContact();
                     break;
+                case 5:
+                    flag = 0;
+                    System.out.println("Exited Successfully");
+                    break;
                 default:
-                    System.out.println("Wrong Operation Number");
+                    System.out.println("Enter a valid Operation Number: ");
                     break;
             }
 
-        } while (option < 5);
+        }
     }
 }
