8a52308cf6705b09365cdb8a87b132fc8e308b6b
gaurav459433 <gaurav459433@gmail.com>
2023-03-21 18:23:43 +0530

    Added: Code For Edit Contact By Using Their Name

--- AddressBook.java
+++ AddressBook.java
@@ -29,4 +29,29 @@ public class AddressBook {
     public void showContact() {
         System.out.println(cp.toString());
     }
+
+    public void editContact() {
+        System.out.println("Enter First Name To Edit:");
+        String name = sc.next();
+        if (cp.getFirstName().equals(name)) {
+            System.out.println("Enter First Name: ");
+            cp.setFirstName(sc.next());
+            System.out.println("Enter Last Name: ");
+            cp.setLastName(sc.next());
+            System.out.println("Enter Address Name: ");
+            cp.setAddress(sc.next());
+            System.out.println("Enter City Name: ");
+            cp.setCity(sc.next());
+            System.out.println("Enter State Name: ");
+            cp.setState(sc.next());
+            System.out.println("Enter Email: ");
+            cp.setEmail(sc.next());
+            System.out.println("Enter Zip Code: ");
+            cp.setZip(sc.nextInt());
+            System.out.println("Enter Mobile Number: ");
+            cp.setPhoneNumber(sc.nextLong());
+        } else {
+            System.out.println("Contact Doesn't Exist:");
+        }
+    }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,10 +1,33 @@
 package addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book Program: ");
+        Scanner scan = new Scanner(System.in);
         AddressBook book = new AddressBook();
-        book.addContact();
-        book.showContact();
+        int option;
+
+        do {
+            System.out.println("1. ADD CONTACT \n2. EDIT CONTACT \n3. DISPLAY CONTACT ");
+            System.out.println("Enter the Operation Number");
+            option = scan.nextInt();
+            switch (option) {
+                case 1:
+                    book.addContact();
+                    break;
+                case 2:
+                    book.editContact();
+                    break;
+                case 3:
+                    book.showContact();
+                    break;
+                default:
+                    System.out.println("Wrong Operation Number");
+                    break;
+            }
+
+        }while (option < 4);
     }
 }
